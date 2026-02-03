d3c9877e562364446dbb36f55e7c8060e1941a37
Tran Son <tranyson.hhht@gmail.com>
2026-02-03 22:30:39 +0700

    add print curent date

--- test.js
+++ test.js
@@ -2,3 +2,6 @@ console.log("This is a test file.");
 const _ = require('lodash');
 const arr = [1, 2, 3, 4, 5];
 console.log(_.shuffle(arr));
+
+const dayjs = require('dayjs');
+console.log("Current Date and Time:", dayjs().format());
